0cfe66db10d89499525e14a4e76679637694f041
MAYARA MARTINS DE ANDRADE <25203738@dsn127m1241955.cfp127aluno.local>
2026-02-24 11:30:57 -0300

    feat: adicionando funcoes, novos inputs e botao deletar

--- main.js
+++ main.js
@@ -3,11 +3,35 @@
 function adicionarProduto(){
     const lista = document.getElementById('lista')
     const produto = document.getElementById('produto')
+    const codigo = document.getElementById('codigo')
+    const quantidade = document.getElementById('quantidade')
 
-    //cria o span e pega o elemento dele
-    const span = document.createElement('span')
-    span.textContent = produto.value
-    span.className = 'bg-blue-100 px-8 py-2'
+    //condicional para verificar se o campo está vazio.
+    if(produto.value == '' || codigo.value == '' || quantidade.value == ''){ //se o campo estiver vazio, emite um aleta na tela e volta o ponteiro para o campo
+        alert('teste')
+        produto.focus()
+        codigo.focus()
+        quantidade.focus()
+    }else{ // se estiver preenchido cria o objeto apaga o que está escrito anteriormente e volta o ponteiro após o click no botao adicionar
+        //cria o span e pega o elemento dele
+        const spanProduto = document.createElement('span')
+        spanProduto.textContent = produto.value
+        spanProduto.className = 'flex flex-col bg-blue-100 px-8 py-2'
 
-    lista.appendChild(span)
+        const spanCodigo = document.createElement('span')
+        spanCodigo.textContent = codigo.value
+        spanCodigo.className = 'flex flex-col bg-blue-100 px-8 py-2'
+
+        const spanQuantidade = document.createElement('span')
+        spanQuantidade.textContent = codigo.value
+        spanQuantidade.className = 'flex flex-col bg-blue-100 px-8 py-2'
+
+        lista.appendChild(spanProduto)
+        lista.appendChild(spanCodigo)
+        lista.appendChild(spanQuantidade)
+        produto.value = ""
+        codigo.value = ''
+        quantidade.value = ''
+        produto.focus()
+    }
 }
